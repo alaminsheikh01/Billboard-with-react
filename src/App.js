45605483b17718128d56a1655cfe08090b0ec3e9
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-10-10 00:55:38 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import logo1 from "./asset/images/logo.png";
+
 function App() {
   return (
     <>
       <section className="nav_custom">
-        <div className="logo"></div>
+        <div className="logo">
+          <img src={logo1} alt="logo image" />
+        </div>
         <div className="mid_logo"></div>
         <div className="right_content"></div>
       </section>
